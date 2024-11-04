24fa84715f54a49ec282c025e76220bc8bdd9991
joaoclink221 <joaoclink221@gmail.com>
2024-11-04 11:23:06 -0300

    ok

--- frontend/src/components/modais/modalCadastro/ModalPaciente.jsx
+++ frontend/src/components/modais/modalCadastro/ModalPaciente.jsx
@@ -6,50 +6,84 @@ const ModalPaciente = () => {
                 <div className="Dados-iniciais">
                     <h1>Dados-iniciais</h1>
                     <div className="heimdal">
-                    <div>
-                        <label htmlFor="name">Nome</label>
-                        <div>
-                            <input type="text" />
+                        <div className="sla">
+                            <label htmlFor="name" >Nome</label>
+                            <div >
+                                <input type="text" className="mimir" />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label htmlFor="date">Data de Nascimente</label>
-                        <div>
-                            <input type="Date" />
+                        <div >
+                            <label htmlFor="date" className="sla">Data de Nascimente</label>
+                            <div>
+                                <input type="Date" className="loki" />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <label htmlFor="name">Sexo</label>
-                        <div>
-                            <select name="filter" id="filtro">
-                                <option value="">Feminino</option>
-                                <option value="">Masculino</option>
-                            </select>
-                        </div>
+                        <div className="sla">
+                            <label htmlFor="name">Sexo</label>
+                            <div>
+                                <select name="filter"
+                                    id="filtro" >
+                                    <option value="">Feminino</option>
+                                    <option value="">Masculino</option>
+
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="Dados da Consulta">
-                    <div>
-                        <label htmlFor="name">Data da Consulta</label>
-                        <div>
-                            <input type="date" />
+                <hr />
+
+                <div className="Dados-da-Paciente">
+                    <h1>Informações</h1>
+                    <div className="control">
+                        <div className="info-dieta">
+                            <div className="sla">
+                                <label htmlFor="">Cintura</label>
+                                <input type="text" className="noname" />
+                            </div>
+                            <div className="sla">
+                                <label htmlFor="">Quadril</label>
+                                <input type="text" className="noname" />
+                            </div>
+                            <div className="sla">
+                                <label htmlFor="">Altura</label>
+                                <input type="text" className="noname" />
+                            </div>
+                            <div className="sla">
+                                <label htmlFor="">Peso</label>
+                                <input type="text" className="noname" />
+                            </div>
+                        </div>
+                        <div className="info-dieta">
+                            <div className="sla">
+                                <label htmlFor="">Tipo de dieta</label>
+                                <input type="text" className="noname" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="date">Data de Inicio </label>
+                                <div>
+                                    <input type="Date" className="loki" />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="date">Conclusão</label>
+                                <div>
+                                    <input type="Date" className="loki" />
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <label htmlFor="name">Horário</label>
-                        <div>
-                            <input type="text" />
+                    <div className="atlas">
+                        <div className="nem">
+                            <label><h1>Descrição do paciente</h1></label>
+                            <textarea type="text"
+                                className="description" />
                         </div>
-                    </div>
-                    <div>
-                        <label htmlFor="name">Tipo de Consulta</label>
-                        <div>
-                        <select name="filter" id="filtro">
-                                <option value="">Presencial</option>
-                                <option value="">Online</option>
-                            </select>
+                        <div className="mid">
+                            <button className="bnt">Salvar</button>
+                            <button className="bnt">excluir</button>
                         </div>
                     </div>
                 </div>
