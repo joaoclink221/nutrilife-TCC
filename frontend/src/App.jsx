9f7ad03090e31ba62722963701223c61acd8a876
TUTUTEO <ra55569201840@acaonsfatima.org.br>
2024-11-07 10:36:12 -0300

    sla

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import './app.scss';
-import {HeaderLanding} from "./components/header2/Header2"
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
 
 
 
