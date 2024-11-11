04c5e4256d74b14bbadf6aafab4d9f15510aef51
ferreiraluiza <annaluizaferreira1306@gmail.com>
2024-11-11 09:43:38 -0300

    mixi ;/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,16 +22,28 @@ export const App = () => {
         </p>
       </div>
 
-      <div className='Cards'>
+      <div className='section2'>
+
         <h1 className='titulo2'>A NUTRI LIFE</h1>
 
-        <div className='card1'><p>Ajudará você a tornar seu estilo de vida mais saudável</p></div>
+        <div className='Cards'>
+          <div className='card1'><p>Ajudará você a tornar seu estilo de vida mais saudável</p></div>
 
-        <div className='card2'> <p>Entender melhor sobre principais assuntos sobre nutrição</p></div>
+          <div className='card2'> <p>Entender melhor sobre principais assuntos sobre nutrição</p></div>
 
-        <div className='card3'><p>Aumento de energia e vitalidade tornando sau vida mais longa</p></div>
+          <div className='card3'><p>Aumento de energia e vitalidade tornando vida mais longa</p></div>
 
-        <div className='card4'><p>Conultas e exames pensados em seu bem-estar, por um preço acessível</p></div>
+          <div className='card4'><p>Consultas e exames pensados em seu bem-estar, por um preço acessível</p></div>
+        </div>
+      </div>
+
+      <div className='section3'>
+        <img src="/src/assets/image/image2.jpg" alt="image2" />
+        <h1 className='titulo3'>Transforme Sua Vida com Nutrição Saudável</h1>
+        <p className='texto'>Agende sua consulta, exames, obtenha seus resultados e seja acompanhado em sua dieta personalizada <br />
+          Acompanhe relatórios, com recomendações e sugestões </p>
+
+        <button className='agendar'>Agendar Consulta</button>
       </div>
 
     </div>
