af9c83151582ac1edfb4214a8eb8088a6c6c9a97
João Victor <ra46791342838@acaonsfatima.org.br>
2024-11-18 01:56:01 -0300

    ok

--- frontend/src/pages/financeiro/financeiro.jsx
+++ frontend/src/pages/financeiro/financeiro.jsx
@@ -1,41 +1,48 @@
 import './Financeiro.scss';
 import Header2 from "../../components/header2/Header2.jsx"
-import { ArrowUp } from "lucide-react"
-import { ArrowDown } from "lucide-react"
-import { HandCoins } from "lucide-react"
-import { ThumbsUp } from "lucide-react"
-import { PlusIcon } from "lucide-react"
+import { ArrowDown, HandCoins, ThumbsUp, ThumbsDown, PlusIcon, ArrowUp, Trash } from "lucide-react"
 import ModalFinanceiro from '../../components/modais/ModalFinanceiro/ModalFinanceiro.jsx';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-function Financeiro(){
-  const [ isModalOpen, setIsModalOpen] = useState(false)
+function Financeiro() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const [despesasList, setDespesasList] = useState([])
   const [erro, setErro] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     axios.get("http://localhost:5010/listarDespesas")
-    .then(response =>{
-      setDespesasList(response.data)
-      setErro(null)
-    })
-    .catch(error => {
-      console.error("erro ao buscar conta:", error);
-      if(despesasList.length == 0){setErro("nenhuma despesa encontrada")}
-        else {setErro("erro ao buscar despesa. tente novamente mais tarde")}
-    })
-  }, [despesasList])
+      .then(response => {
+        setDespesasList(response.data)
+        setErro(null)
+      })
+      .catch(error => {
+        console.error("erro ao buscar conta:", error);
+        if (despesasList.length == 0) { setErro("nenhuma despesa encontrada") }
+        else { setErro("erro ao buscar despesa. tente novamente mais tarde") }
+      })
+  }, [])
+
+  const excluiDespesa = async (id) => {
+    try {
+      const resposta = await axios.delete(`http://localhost:5010/excluirDespesas/${id}`);
+      alert(resposta.data.message);
+      setDespesasList(despesasList.filter((despesa) => despesa.id_despesa !== id))
+    } catch (erro) {
+      alert("Erro ao excluir consulta: " + erro.message);
+    }
+  }
   return (
     <div className='vô-financeiro'>
       <Header2 />
 
       <div className="pai-financeiro">
 
-        <button className='circleModal'>
+        <button className='circleModal' onClick={() => setIsModalOpen(true)}>
           <PlusIcon className="iconM" />
         </button>
 
+        <ModalFinanceiro isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
         <div className='tabela'>
           <table className="table table hover">
             <thead>
@@ -47,11 +54,19 @@ function Financeiro(){
               </tr>
             </thead>
             <tbody>
-              <tr className='tren'>
-                <td scope='col'><ThumbsUp className="icon1" /></td>
-                <td scope='col'>conta de luz</td>
-                <td scope='col'>800,9</td>
-              </tr>
+              {despesasList.map((despesa, index) => (
+                <tr key={index} className='tren'>
+                  <td scope='col'>{despesa.situacao ? <ThumbsUp /> : <ThumbsDown />}</td>
+                  <td scope='col'>{despesa.ds_despesa}</td>
+                  <td scope='col'>{despesa.valor}</td>
+                  <td scope="col">
+                    <button onClick={() => excluiDespesa(despesa.id_despesa)} className='btn-excluir'>
+                      <Trash />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+
             </tbody>
           </table>
         </div>
