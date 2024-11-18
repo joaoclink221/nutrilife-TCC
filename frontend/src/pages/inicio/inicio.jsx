ca76b659fc951e7e067c285ae8cca8e7b8be5c7d
joaoclink221 <ra46791342838@acaonsfatima.org.br>
2024-11-18 09:43:37 -0300

    inico feito

--- frontend/src/pages/inicio/inicio.jsx
+++ frontend/src/pages/inicio/inicio.jsx
@@ -1,12 +1,57 @@
+import './inicio.scss';
+import { Link } from 'react-router-dom';
+import { Calendar, Mail, ClipboardList, BadgeDollarSign } from "lucide-react"
+
 const Inicio = () => {
   return (
-    <div className="Hashirama">
-      <header className='filho'>
-        <img src="/src/assets/image/image.png" alt="" />
-        <Link to="/login"> <button className='login'>Login</button></Link>
-      </header>
 
-    </div>
+    <div className='pai'>
+      <div className='barra'>
+        <img src="/src/assets/image/image.png" alt="" />
+
+      </div>
+      <div className='qq1'>
+        <div className='q1'>
+          <div className='quadro1'>
+            <Link to='/agenda'>
+              <button className='boo'>
+                <Calendar />
+                <h1>Agenda</h1>
+              </button>
+            </Link>
+          </div>
+
+          <div className='quadro1'>
+            <Link to='' >
+              <button className='boo'>
+                <Mail />
+                <h1>E-mail</h1>
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className='q1'>
+          <div className='quadro1'>
+            <Link to='/pacientes'>
+              <button className='boo'>
+                <ClipboardList />
+                <h1>Paciente</h1>
+              </button>
+            </Link>
+          </div>
+
+          <div className='quadro1'>
+            <Link to='/financeiro'>
+              <button className='boo'>
+                <BadgeDollarSign />
+                <h1>Financeiro</h1>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div >
   )
 }
 
